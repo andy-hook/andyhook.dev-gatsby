fe3dbe21322e57bfd342a4d039367e348de29661
Andy Hook <hello@andy-hook.co.uk>
2019-04-13 19:42:32 +0100

    Convert layout to use static query hook

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,48 +5,55 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React, {ReactNode} from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import React, { ReactNode } from "react"
 
 import Header from "./header"
 import "./layout.css"
 
-type Props = {
+interface Props {
   children: ReactNode
 }
 
-const Layout = ({ children }: Props) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+interface LayoutData {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  }
+}
+
+const Layout: React.SFC<Props> = ({ children }) => {
+  const data: LayoutData = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
 
 export default Layout
