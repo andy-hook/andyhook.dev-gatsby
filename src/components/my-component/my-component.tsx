5024e1d1d23a2566216a4a6fd2f6b967b96fad70
Andy Hook <hello@andy-hook.co.uk>
2019-04-14 16:39:50 +0100

    Add web font loader with typekit id

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import styled from "styled-components"
 
 const StyleTest = styled.div`
+  font-family: montserrat, sans-serif;
+
+  font-weight: 600;
+  font-style: normal;
   padding: 50px;
   background-color: orange;
 `
