74181c670658d71d191049127b8fbbbb08317978
Andy Hook <hello@andy-hook.co.uk>
2019-04-14 21:01:44 +0100

    Fix issue with unrendered layout wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,5 @@ import React from "react"
 import Layout from "./src/components/layout"
 
 export const wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
   return <Layout {...props}>{element}</Layout>
 }
