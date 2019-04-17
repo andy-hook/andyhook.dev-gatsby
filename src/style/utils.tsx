a2340566d59050cfe18514128eb3f59ad9424f33
Andy Hook <hello@andy-hook.co.uk>
2019-04-17 23:41:51 +0100

    uniformScale tests

--- src/style/utils.tsx
+++ src/style/utils.tsx
@@ -1,16 +1,20 @@
 import { breakpoints } from "./mq"
-import { stripUnit, getValueAndUnit } from "polished"
+import { stripUnit } from "polished"
 import styleVars from "./variables"
 
-export const uniformScale = (cssValue: string, targetMediaQuery: string) => {
+export const uniformScale = (
+  cssValue: string,
+  targetMediaQuery: string
+): string => {
   // Split into value and unit
   const value = stripUnit(cssValue)
-  const unit = getValueAndUnit(cssValue)[1]
+  const unit = stripUnit(cssValue, true)[1]
+  const breakpoint = breakpoints[targetMediaQuery]
 
   // Convert from relative to px value
   const convertedUnit = unit === "px" ? value : value * styleVars.baseFontSize
 
-  const bpValue = stripUnit(breakpoints[targetMediaQuery])
+  const bpValue = stripUnit(breakpoint)
 
   return `${convertedUnit / (bpValue * 0.01 * 1)}vw`
 }
