1664ff5cb07982f886a731df34c4bc99b883a217
Andy Hook <hello@andy-hook.co.uk>
2019-04-29 00:24:33 +0100

    Fix build

--- src/components/social/social.stories.tsx
+++ src/components/social/social.stories.tsx
@@ -3,8 +3,7 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import Social from "./social"
 import { socialIcons } from "../../mock-data"
-import { themes } from "@storybook/theming"
 
-storiesOf("Social", module)
-  .addParameters({ options: { theme: themes.dark } })
-  .add("with text", () => <Social items={socialIcons} />)
+storiesOf("Social", module).add("with text", () => (
+  <Social items={socialIcons} />
+))
