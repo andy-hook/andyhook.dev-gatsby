83a371280a9053a4b3d97cc5b5817a8bcc442aef
Andy Hook <hello@andy-hook.co.uk>
2019-05-11 19:26:49 +0100

    Add initial project pages

---
+++ src/pages/jamieson.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Project from "@components/project/project"
+
+import SEO from "@components/seo"
+
+const BrandwatchProject: React.FunctionComponent = () => {
+  return (
+    <>
+      <SEO />
+      <Project>hello</Project>
+    </>
+  )
+}
+
+export default BrandwatchProject
