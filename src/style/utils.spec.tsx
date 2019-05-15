2e9784f042308af130eb1f7ab1ef6922f20def51
Andy Hook <hello@andy-hook.co.uk>
2019-05-15 23:57:35 +0100

    Stricter style util tests

--- src/style/utils.spec.tsx
+++ src/style/utils.spec.tsx
@@ -9,14 +9,14 @@ describe("uniformScale", () => {
   })
 
   test("Should return correct value when supplied PX unit", () => {
-    expect(uniformScale("10px", "topUltra")).toEqual("0.45454545454545453vw")
+    expect(uniformScale("10px", "topUltra")).toMatch("0.45454545454545453vw")
   })
 
   test("Should return correct value when supplied EM unit", () => {
-    expect(uniformScale("10em", "topUltra")).toEqual("7.2727272727272725vw")
+    expect(uniformScale("10em", "topUltra")).toMatch("7.2727272727272725vw")
   })
 
   test("Should return correct value when supplied REM unit", () => {
-    expect(uniformScale("10rem", "topUltra")).toEqual("7.2727272727272725vw")
+    expect(uniformScale("10rem", "topUltra")).toMatch("7.2727272727272725vw")
   })
 })
