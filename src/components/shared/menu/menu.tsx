d03cb93549312e6125b0881356e2970df4113b8e
Andy Hook <hello@andy-hook.co.uk>
2019-05-15 23:04:56 +0100

    Initial menu animation hooks

--- src/components/shared/menu/menu.tsx
+++ src/components/shared/menu/menu.tsx
@@ -1,12 +1,39 @@
-import React, { memo } from "react"
+import React, { memo, useEffect } from "react"
 import styled from "styled-components"
+import { Ref } from "@custom-types/ref"
+import { TweenMax, Expo } from "gsap"
 
 interface Props {
   open?: boolean
 }
 
 const Menu: React.FunctionComponent<Props> = memo(({ open }) => {
-  return <MenuContainer open={open} />
+  const containerRef = React.useRef() as Ref
+
+  const animateOpen = () => {
+    TweenMax.to(containerRef.current, 0.5, {
+      ease: Expo.easeOut,
+      y: "0%",
+    })
+  }
+
+  const animateClose = () => {
+    TweenMax.to(containerRef.current, 0.5, {
+      ease: Expo.easeOut,
+      y: "-100%",
+      clearProps: "transform",
+    })
+  }
+
+  const triggerAnimation = () => {
+    return open ? animateOpen() : animateClose()
+  }
+
+  useEffect(() => {
+    triggerAnimation()
+  })
+
+  return <MenuContainer open={open} ref={containerRef} />
 })
 
 const MenuContainer = styled.div<Props>`
@@ -17,13 +44,7 @@ const MenuContainer = styled.div<Props>`
   height: 100%;
   z-index: 50;
   background-color: white;
-  transform: translate3d(-100%, 0, 0);
-
-  ${({ open }) =>
-    open &&
-    `
-    transform: translate3d(0, 0, 0);
-  `}
+  transform: translate3d(0, -100%, 0);
 `
 
 export default Menu
