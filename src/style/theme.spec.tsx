64903a0e8225eddeedb13a55ebb0132e2fcde46e
Andy Hook <hello@andy-hook.co.uk>
2019-05-23 01:10:14 +0100

    Initial theme style util tests

---
+++ src/style/theme.spec.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { mountWithTheme } from "@test-utils"
+import {
+  themeToneAlpha,
+  themeTone,
+  themeText,
+  themeTextAlpha,
+} from "@style/theme"
+import styled from "styled-components"
+import "jest-styled-components"
+
+const ThemeToneComponent = styled.div`
+  background-color: ${themeTone(100)};
+`
+
+const ThemeToneAlphaComponent = styled.div`
+  background-color: ${themeToneAlpha(100, 0)};
+`
+
+const ThemeTextComponent = styled.div`
+  color: ${themeText(100)};
+`
+
+const ThemeTextAlphaComponent = styled.div`
+  color: ${themeTextAlpha(100, 0)};
+`
+
+describe("themeTone", () => {
+  it("renders correct hsl from given tone value", () => {
+    const tree = mountWithTheme(<ThemeToneComponent />)
+    expect(tree).toHaveStyleRule("background-color", "hsl(240,17%,2%)")
+  })
+})
+
+describe("themeToneAlpha", () => {
+  it("renders correct hsla from given tone value", () => {
+    const tree = mountWithTheme(<ThemeToneAlphaComponent />)
+    expect(tree).toHaveStyleRule("background-color", "hsla(240,17%,2%,0)")
+  })
+})
+
+describe("themeText", () => {
+  it("renders correct hsl from given text value", () => {
+    const tree = mountWithTheme(<ThemeTextComponent />)
+    expect(tree).toHaveStyleRule("color", "hsl(240,2%,100%)")
+  })
+})
+
+describe("themeTextAlpha", () => {
+  it("renders correct hsla from given text value", () => {
+    const tree = mountWithTheme(<ThemeTextAlphaComponent />)
+    expect(tree).toHaveStyleRule("color", "hsla(240,2%,100%,0)")
+  })
+})
