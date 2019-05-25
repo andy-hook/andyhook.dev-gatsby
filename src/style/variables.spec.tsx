bf0fb084e7a2e36dfa340ce157f2fbadc8ee09a9
Andy Hook <hello@andy-hook.co.uk>
2019-05-25 15:51:14 +0100

    Fix tests

--- src/style/variables.spec.tsx
+++ src/style/variables.spec.tsx
@@ -2,13 +2,13 @@ import { lightGrey, darkGrey, lightGreyAlpha, darkGreyAlpha } from "./variables"
 
 describe("lightGrey", () => {
   test("should return correct light grey value as hsl string", () => {
-    expect(lightGrey(100)).toEqual("hsl(240, 2%, 100%)")
+    expect(lightGrey(100)).toEqual("hsl(240, 3%, 100%)")
   })
 })
 
 describe("lightGreyAlpha", () => {
   test("should return correct light grey value as hsla string", () => {
-    expect(lightGreyAlpha(100, 50)).toEqual("hsla(240, 2%, 100%,50)")
+    expect(lightGreyAlpha(100, 50)).toEqual("hsla(240, 3%, 100%,50)")
   })
 })
 
