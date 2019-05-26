c1d6063c148fe8af2c85fe62f5a47d48538b0543
Andy Hook <hello@andy-hook.co.uk>
2019-05-26 17:41:50 +0100

    Correct grey type issue

--- src/types/theme.tsx
+++ src/types/theme.tsx
@@ -1,15 +1,4 @@
-export type TGrey =
-  | 000
-  | 100
-  | 200
-  | 300
-  | 400
-  | 500
-  | 600
-  | 700
-  | 800
-  | 900
-  | 1000
+export type TGrey = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 1000
 
 export type TLayer = "lowest" | "low" | "medium" | "high" | "highest"
 
