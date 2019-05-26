f9ac358e493508a025542b141f2ef4ce46d91b02
Andy Hook <hello@andy-hook.co.uk>
2019-05-26 17:41:39 +0100

    Fix menu test

--- src/components/shared/menu/menu.spec.tsx
+++ src/components/shared/menu/menu.spec.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import { shallowWithTheme } from "@test-utils"
+import { socialIcons, projectsData } from "@mock-data"
 
 import Menu from "./menu"
 
 describe("<Menu />", () => {
   it("renders correctly", () => {
-    const tree = shallowWithTheme("dark", <Menu />)
+    const tree = shallowWithTheme(
+      "dark",
+      <Menu projects={projectsData} social={socialIcons} />
+    )
     expect(tree).toMatchSnapshot()
   })
 })
