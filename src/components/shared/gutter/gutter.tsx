a4841c9d77ecca37b0a8154c97e7fb2296644888
Andy Hook <hello@andy-hook.co.uk>
2019-05-27 01:29:30 +0100

    Adjust gutter

--- src/components/shared/gutter/gutter.tsx
+++ src/components/shared/gutter/gutter.tsx
@@ -1,5 +1,6 @@
 import React, { memo, ReactNode } from "react"
 import styled from "styled-components"
+import { mq } from "@style/utils"
 
 interface Props {
   children: ReactNode
@@ -10,8 +11,13 @@ const Gutter: React.FunctionComponent<Props> = memo(({ children }) => {
 })
 
 const GutterContainer = styled.div`
-  padding-left: 5%;
-  padding-right: 5%;
+  padding-left: 8%;
+  padding-right: 8%;
+
+  ${mq.greaterThan("topPalm")`
+    padding-left: 5%;
+    padding-right: 5%;
+  `}
 
   width: 100%;
 `
