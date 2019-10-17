49e63b9b86b6e360add43310fbbaae3e492da38c
Andy Hook <hello@andy-hook.co.uk>
2019-10-17 20:05:55 +0100

    Add additional elements to ref type

--- src/types/ref.tsx
+++ src/types/ref.tsx
@@ -3,4 +3,6 @@ import React from "react"
 export type Ref = React.MutableRefObject<HTMLImageElement> &
   React.MutableRefObject<HTMLAnchorElement> &
   React.MutableRefObject<HTMLSpanElement> &
-  React.MutableRefObject<HTMLDivElement>
+  React.MutableRefObject<HTMLDivElement> &
+  React.MutableRefObject<HTMLButtonElement> &
+  React.MutableRefObject<SVGRectElement>
