e5bbaa027e0cbe36dbf48d929ebe697dbe1b4830
Andy Hook <hello@andy-hook.co.uk>
2019-12-10 18:30:49 +0000

    Fix build

--- src/components/project/header/header.tsx
+++ src/components/project/header/header.tsx
@@ -2,7 +2,6 @@ import React, { MutableRefObject } from "react"
 import { ProjectItem } from "@custom-types/model"
 import CoverImageContainer from "@components/shared/cover-image/cover-image.container"
 import * as S from "./header.style"
-import usePageTransition from "@hooks/page-transition"
 
 interface Props {
   project: ProjectItem
@@ -11,23 +10,8 @@ interface Props {
 const Header: React.FunctionComponent<Props> = ({ project }) => {
   const backgroundRef = React.useRef() as MutableRefObject<HTMLElement>
 
-  const animatePop = () => {}
-
-  const animateEnter = () => {}
-
-  const animateExit = () => {}
-
-  const { inviewRef } = usePageTransition({
-    runInview: {
-      onEnter: animateEnter,
-      onExit: animateExit,
-      onPop: animatePop,
-      onEnterFromMenu: animateEnter,
-    },
-  })
-
   return (
-    <S.Container ref={inviewRef}>
+    <S.Container>
       <S.BackgroundImage ref={backgroundRef}>
         <CoverImageContainer imagePath={project.images} />
       </S.BackgroundImage>
