59cdf9c6bf3347e50e7bbe33f27b28f222106082
Andy Hook <hello@andy-hook.co.uk>
2019-12-15 15:10:00 +0000

    Fix build

--- src/components/projects/project-card/project-card.spec.tsx
+++ src/components/projects/project-card/project-card.spec.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import ProjectCard from "./project-card"
 import { shallowWithTheme } from "@test-utils"
+import { mockProjectsData } from "@data/mocks"
+
+const { path, images } = mockProjectsData.brandwatch
 
 describe("<ProjectCard />", () => {
   it("renders correctly", () => {
     const tree = shallowWithTheme(
       "dark",
-      <ProjectCard title="This is a title" />
+      <ProjectCard title="This is a title" path={path} images={images} />
     )
     expect(tree).toMatchSnapshot()
   })
